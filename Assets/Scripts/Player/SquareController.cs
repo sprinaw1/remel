04fa359390026b8a95aa82c99e2ac12d9cf69c8a
Kyle Xiong <kxiong@cs.cmu.edu>
2017-09-18 23:27:21 -0400

    Square test.

---
+++ Assets/Scripts/Player/SquareController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class SquareController : MonoBehaviour {
+
+	[SerializeField]
+	protected float moveSpeed = 10f;
+
+	protected Rigidbody rb;
+
+	protected void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	protected void Update () {
+		KeyboardInput ();
+	}
+
+	private void KeyboardInput() {
+		float h = Input.GetAxis ("Horizontal");
+
+		rb.velocity = h * Vector3.right * moveSpeed;
+	}
+
+}
