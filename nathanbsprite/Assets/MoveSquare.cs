7b574113401c73d6c5834082359fc6e6f5b81fa0
Nathan Blinn <nathan@nathanblinn.com>
2017-09-20 12:11:01 -0400

    Added tutorial

---
+++ nathanbsprite/Assets/MoveSquare.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveSquare : MonoBehaviour
+{
+    public float speed = 0.25f;
+
+    // Update is called once per frame.
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+
+        gameObject.transform.position += h * Vector3.right * speed;
+    }
+}
